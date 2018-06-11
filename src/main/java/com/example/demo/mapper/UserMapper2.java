1962217fbd9833501d69f96e1822ab53365f1e63
wl0927 <1226682591@qq.com>
2018-06-11 16:10:06 +0800

    wl

--- src/main/java/com/example/demo/mapper/UserMapper2.java
+++ src/main/java/com/example/demo/mapper/UserMapper2.java
@@ -5,6 +5,7 @@ import com.example.demo.pojo.User2;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -14,14 +15,10 @@ import java.util.List;
  * @Author 罗曼蒂克
  * @Date Created in 11:03   2018/6/5
  */
-@Mapper
-@Component
+@Repository
 public interface UserMapper2 {
-
-
 
     @Select("SELECT id,loginName, userName,easybuy_user.password  FROM  easybuy_user")
     //@Transactional
     List<User2> userLlist();
-    
 }
